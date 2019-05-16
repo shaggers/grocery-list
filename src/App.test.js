58a2af5ff05f1c66c6d362608a96f3d9a813e874
shaggers <camtl66@gmail.com>
2019-05-15 20:38:48 -0500

    testing initialized

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import RoomList from './components/RoomList';
+import { exportAllDeclaration } from '@babel/types';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
+// Enzyme intro
+
+describe('Examining the syntax of Jest tests', () => {
+   
+  it('sums numbers', () => {
+      expect(1 + 2).toEqual(3);
+      expect(2 + 2).toEqual(4);
+   });
 });
+
+describe('First React component test with Enzyme', () => {
+  it('renders without crashing', () => {
+    const component = Enzyme.shallow(<App />);
+     expect(component).toMatchSnapshot();
+   });
+});
+
+// Verifying Landing page renders
+
+describe('the Room List component renders on Landing', () => {
+
+  it('should render the content from App.js', () => {
+    const component = Enzyme.shallow(<App />);
+    expect(component).toMatchSnapshot();
+  })
+
+  
+// Check room list has the correct value
+
+  it('should render correctly with no props', () => {
+    const component = Enzyme.shallow(<RoomList />);
+    expect(component.text()).toEqual("List of rooms will go here.");
+    expect(component).toMatchSnapshot();
+  });
+  
+})
